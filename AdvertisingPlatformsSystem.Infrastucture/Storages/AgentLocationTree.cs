edea6b8620281798e00fdd301e5a1b4bf1423744
root <victor.shkurko@bk.ru>
2025-08-18 18:24:24 +0300

    added storage AgentLocationMapTree, TreeNode

--- AdvertisingPlatformsSystem.Infrastucture/Storages/AgentLocationTree.cs
+++ AdvertisingPlatformsSystem.Infrastucture/Storages/AgentLocationTree.cs
@@ -11,12 +11,34 @@ public class AgentLocationMapTree
         public string Key { get; private set; }
         private readonly Dictionary<string, TreeNode> _children;
         private readonly HashSet<ulong> _agentsIds;
+        public IReadOnlyCollection<ulong> AgentsIds => _agentsIds;
         
         public TreeNode(string key) 
         {
             Key = key;
             _children = new Dictionary<string, TreeNode>();
             _agentsIds = new HashSet<ulong>();
+        }
+        
+        public TreeNode? GetChildByKey(string key)
+        {
+            return _children.GetValueOrDefault(key);
+        }
+
+        public void AddAgent(ulong agentId)
+        {
+            _agentsIds.Add(agentId);
+        }
+        
+        public TreeNode GetOrAddChildNode(string key) 
+        {
+            if (!_children.TryGetValue(key, out var node))
+            {
+                node = new TreeNode(key);
+                _children.Add(key, node);
+            }
+            
+            return node;
         }
     }
 }
